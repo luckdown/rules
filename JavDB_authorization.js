8ec9bd39a57e176ce874d5924e23cf27d8a5079f
luckdown <1292139793@qq.com>
2022-04-27 00:39:09 +0800

    Create JavDB_authorization.js

---
+++ JavDB_authorization.js
@@ -0,0 +1,3 @@
+let headers = $request.headers;
+headers['authorization'] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6OTgwNDk5LCJ1c2VybmFtZSI6Imlvc2hrajEifQ.aSlLcMcGxEtNrsxife9exe0BGs6C4bQHdiBUaYamEAM';
+$done({headers});
