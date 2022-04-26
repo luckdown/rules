ccd432236564be021b048210c1d17c0fad0f6792
luckdown <1292139793@qq.com>
2022-04-26 21:39:25 +0800

    Create JavDB_users.js

---
+++ JavDB_users.js
@@ -0,0 +1,27 @@
+let body = JSON.parse($response.body);
+body = {
+  "success": 1,
+  "message": null,
+  "data": {
+    "user": {
+      "id": 900000,
+      "promotion_days": 999,
+      "checkin_days": 0,
+      "want_watch_count": 0,
+      "promotion_code": "s8rvea",
+      "vip_expired_at": "2099-09-09T22:16:31.000+08:00",
+      "username": "ios黑科技",
+      "share_url": "https://jav3.app/?source=s8rvea",
+      "last_checkin_at": null,
+      "promote_users_count": 999,
+      "email": "ioshkj@163.com",
+      "is_vip": true,
+      "watched_count": 0
+    },
+    "banner_type": "payment"
+  },
+  "action": null
+};
+body = JSON.stringify(body);
+$notification.post('修改body','修改body','body =json');
+$done({body}); 
