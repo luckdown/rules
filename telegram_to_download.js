37c2b35f61604ae84bfb2a8312a05d44ecd291c4
luckdown <1292139793@qq.com>
2022-06-24 00:45:37 +0800

    Update telegram_to_download.js

--- telegram_to_download.js
+++ telegram_to_download.js
@@ -1,2 +1,3 @@
-$surge.setSelectGroupPolicy('📲 电报消息', '🔽 下载节点')
-$notification.post('title 切换到下载节点', 'subtitle 切换到下载节点', 'body 切换到下载节点')
+$surge.setSelectGroupPolicy('📲 电报消息', '🔽 下载节点');
+// $notification.post('title 切换到下载节点', 'subtitle 切换到下载节点', 'body 切换到下载节点');
+$done();
