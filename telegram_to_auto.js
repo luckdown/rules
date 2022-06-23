cb987bdba704e9266673b07bd031bf5b9013db2e
luckdown <1292139793@qq.com>
2022-06-24 00:44:31 +0800

    Update telegram_to_auto.js

--- telegram_to_auto.js
+++ telegram_to_auto.js
@@ -1,2 +1,3 @@
-$surge.setSelectGroupPolicy('📲 电报消息', '♻️ 自动选择')
-$notification.post('title 切换到♻️ 自动选择', 'subtitle 切换到♻️ 自动选择', 'body 切换到♻️ 自动选择')
+$surge.setSelectGroupPolicy('📲 电报消息', '🚀 节点选择');
+// $notification.post('title 切换到🚀 节点选择', 'subtitle 切换到🚀 节点选择', 'body 切换到🚀 节点选择');
+$done();
