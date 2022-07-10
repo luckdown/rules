9975058578a4ee2a0c511cac227f7bfc590fc51b
luckdown <1292139793@qq.com>
2022-07-10 19:51:54 +0800

    Update pay_success.js

--- pay_success.js
+++ pay_success.js
@@ -1,1 +1,9 @@
-{"code":200,"msg":"已支付成功！","data":true,"total":0}
+let obj = JSON.parse($response.body);
+obj = {
+    "code": 200,
+    "msg": "已支付成功！",
+    "data": true,
+    "total": 0
+}
+;
+$done({body: JSON.stringify(obj)});
